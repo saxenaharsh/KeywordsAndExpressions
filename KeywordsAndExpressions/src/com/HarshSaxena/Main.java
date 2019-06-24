20196bc38b005ea6e0f1622a0b1a70538c566db1
saxenaharsh <harsh.saxena@gmail.com>
2019-06-24 17:24:21 +0530

    Add files via upload

---
+++ KeywordsAndExpressions/src/com/HarshSaxena/Main.java
@@ -0,0 +1,19 @@
+package com.HarshSaxena;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //a mile is equal to 1.609344
+
+        double kiloMeter = (100 * 1.609344);
+        System.out.println("100 Mile is equal to " + kiloMeter + " Kilometers");
+
+        int highScore = 50;
+
+        if(highScore == 50){
+            System.out.println("This is an expression");
+        }
+
+    }
+}
